e5ecca35ac5136fe333745219768b633336076ae
Kevin Owino <kowino@STIMA-SAC.COM>
2023-11-01 17:54:45 +0300

    Upload working

--- app/models.py
+++ app/models.py
@@ -21,7 +21,118 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         return self.email
 
+class CarBrand(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['id']),
+        ]
+    name = models.CharField(max_length=100, unique=True)
+    car_brand_date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+def cleanDecimal(value):
+    return float(str(value).replace(",", ""))
+
 class Car(models.Model):
-    make = models.CharField(default="", max_length=100, blank=True, null=True)
-    brand = models.CharField(default="", max_length=100, blank=True, null=True)
-    car_image = models.ImageField(upload_to='car-images/', blank=True, null=True)
+    class Meta:
+        indexes = [
+            models.Index(fields=['id']),
+        ]
+    brand = models.ForeignKey(CarBrand, related_name="brand_cars", on_delete=models.CASCADE, default=None)
+    vendor = models.CharField(max_length=255, default=None)
+    make = models.CharField(max_length=100, unique=False)
+    color = models.CharField(max_length=100, default="")
+    # weight = models.CharField(max_length=100, default="", blank=True)
+    body_type = models.CharField(max_length=100, default="")
+    engine_size = models.CharField(max_length=100, default="")
+    # engine_size = models.CharField(default="-SELECT ENGINE SIZE-", max_length=255, choices=es.ENGINE_SIZES, help_text=es.ENGINE_SIZES)
+    location = models.CharField(max_length=254, default="")
+    car_image = models.ImageField(upload_to="car-images/", blank=True, default="")
+    is_sold = models.BooleanField(default=False)
+    is_featured = models.BooleanField(default=False)
+    description = models.TextField(default="")
+
+    STEERING_TYPE_CHOICES = [
+        ('-SELECT STEERING TYPE-', 
+            (
+                ('RHD', 'RHD'),
+                ('LHD', 'LHD')
+            )
+        ),
+    ]
+    steering_type = models.CharField(default="RHD", max_length=20, choices=STEERING_TYPE_CHOICES)
+
+    CAR_TYPE_CHOICES = [
+        ('-SELECT CAR TYPE-', 
+            (
+                ('NEW CAR', 'NEW CAR'),
+                ('LOCALLY USED', 'LOCALLY USED')
+            )
+        ),
+    ]
+    car_type = models.CharField(default="NEW", max_length=20, choices=CAR_TYPE_CHOICES)
+
+    TRANSMISSION_TYPE_CHOICES = [
+        ('-SELECT TRANSMISSION TYPE-', 
+            (
+                ('AUTOMATIC', 'AUTOMATIC'),
+                ('MANUAL', 'MANUAL')
+            )
+        ),
+    ]
+    transmission = models.CharField(default="MANUAL", max_length=20, choices=TRANSMISSION_TYPE_CHOICES)
+
+    FUEL_TYPE_CHOICES = [
+        ('-SELECT FUEL TYPE-', 
+            (
+                ('PETROL', 'PETROL'),
+                ('DIESEL', 'DIESEL')
+            )
+        ),
+    ]
+    fuel_type = models.CharField(default="PETROL", max_length=20, choices=FUEL_TYPE_CHOICES)
+
+    DRIVE_TYPE_CHOICES = [
+        ('-SELECT DRIVE TYPE-', 
+            (
+                ('2WD', '2WD'),
+                ('4WD', '4WD'),
+                ('AWD', 'AWD')
+            )
+        ),
+    ]
+    drive_type = models.CharField(default="2WD", max_length=10, choices=DRIVE_TYPE_CHOICES)
+    
+    deposit_on_hire_purchase = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True, null=True)
+    doors = models.IntegerField(default=0)
+    seats = models.IntegerField(default=0)
+    mileage = models.IntegerField(default=0)
+    year_of_manufacture = models.CharField(max_length=10, default="", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[cleanDecimal])
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[cleanDecimal])
+    buyer_name = models.CharField(max_length=100, default="", blank=True)
+    buyer_mobile_no = models.CharField(max_length=50, default="", blank=True)
+    posting_paid = models.BooleanField(default=False)
+    posting_transaction_id = models.CharField(max_length=100, default="INITIAL", blank=True)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.make or ''
+
+class ExtraCarImage(models.Model):
+    class Meta:
+        indexes = [
+            models.Index(fields=['id']),
+        ]
+    car = models.ForeignKey(Car, related_name="extracarimages", on_delete=models.CASCADE)
+    car_image = models.ImageField(upload_to="extra-car-images/", blank=True, null=True)
+
+    def __str__(self):
+        return str(self.car_image)
+    
+    # def save(self, *args, **kwargs):
+    #     ut = Utilities()
+    #     ut.image_resize(self.car_image, 450, 300)
+    #     super().save(*args, **kwargs)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 import os
 from django.shortcuts import render, redirect
 
-from app.models import Car
+from app.models import Car, CarBrand
 from .forms import CarForm
 import pandas as pd
 from django.core.files import File
@@ -10,28 +10,103 @@ def index(request):
     context = {}
     return render(request, 'app/index.html', context)
 
+def carHasMainPhoto(car_directory_path):
+    has_main_photo = False
+    main_photo_path = ''
+    main_image = ''
+    file_name = ''
+
+    for file in os.listdir(car_directory_path):
+        file_path = ''.join([car_directory_path, '/', file])
+
+        if os.path.isfile(file_path):
+            parts = file.split('.')
+
+            if len(parts) > 1:
+                name = parts[0]
+                file_name = file
+                main_photo_path = ''.join([car_directory_path, '/', file])
+
+                if name == "1":
+                    has_main_photo = True
+                    main_image = file
+                    main_photo_path = ''.join([car_directory_path, '/', file])
+                    break
+
+    extra_images = []
+    if has_main_photo:
+        for file in os.listdir(car_directory_path):
+            while True:
+                if file == main_image:
+                    break
+
+                extra_image_path = ''.join([car_directory_path, '/', file])
+                extra_images.append(extra_image_path)
+
+                break
+
+    return {
+        'has_main_photo': has_main_photo,
+        'file_name': file_name,
+        'extra_images': extra_images,
+        'main_photo_path': main_photo_path
+    }
 
 def bulk_upload_items(request):
     if request.method == 'POST':
         form = CarForm(request.POST, request.FILES)
-        if form.is_valid():
-            excel_file = request.FILES['excel_file']
-            if excel_file.name.endswith('.xls') or excel_file.name.endswith('.xlsx'):
-                df = pd.read_excel(excel_file)
-                for index, row in df.iterrows():
-                    item = Car(make=row['make'], brand=row['brand'])
+        # if form.is_valid():
+        excel_file = request.FILES['excel_file']
+        upload_directory = r'C:/msacar_uploads'
 
-                    filename = os.fsdecode(str(row['car_image']))
-                    print('car_image : ' + str(row['car_image']))
-                    print('filename : ' + str(filename))
-                    file = open(filename, 'rb')
-                    item.car_image.save('image.png', File(file))  # Make sure the column name matches the one in your Excel file
+        cars_directory = ''.join([upload_directory, '/', 'cars'])
+        cars_directory_exists = os.path.exists(cars_directory)
+
+        car_directories = []
+        if cars_directory_exists:
+            for car_directory in os.listdir(cars_directory):
+                car_directories.append(car_directory)
+
+                # car_folder_path = ''.join([cars_directory, '/', car_directory])
+                # for car in os.listdir(car_folder_path):
+                #     print(car)
+                # print("=================")
+                # if excel_file.name.endswith('.xls') or excel_file.name.endswith('.xlsx'):
+            df = pd.read_excel(excel_file, 'CARS')
+            for index, row in df.iterrows():
+                car_folder_path = car_folder_path = ''.join([cars_directory, '/', row['directory']])
+                response = carHasMainPhoto(car_folder_path)
+
+                if response['has_main_photo']:
+                # for car_img in os.listdir(car_folder_path):
+                    car = Car()
+                    car.vendor = "Vendor"
+                    car.make = row['make']
+                    car.body_type =  row['body_type']
+
+                    car_brand = CarBrand.objects.get(pk=row['brand_id'])
+
+                    car.brand = car_brand
+                    file = open(response['main_photo_path'], 'rb')
+                    car.car_image.save(response['file_name'], File(file))
                     file.close()
-                    item.save()
-                    
-                return redirect('app:bulk-upload')  # Redirect to a view that lists the uploaded items
-        else:
-            print('errors : ' + str(form.errors))
+
+                    car.save()
+                    print(car)
+                print("============")
+            # item = Car(make=row['make'], brand=row['brand'])
+
+                #         filename = os.fsdecode(str(row['car_image']))
+                #         print('car_image : ' + str(row['car_image']))
+                #         print('filename : ' + str(filename))
+                #         file = open(filename, 'rb')
+                #         item.car_image.save('image.png', File(file))  # Make sure the column name matches the one in your Excel file
+                #         file.close()
+                #         item.save()
+                        
+            return redirect('app:bulk-upload')  # Redirect to a view that lists the uploaded items
+            # else:
+            #     print('errors : ' + str(form.errors))
     else:
         form = CarForm()
     return render(request, 'app/upload.html', {'form': form})
